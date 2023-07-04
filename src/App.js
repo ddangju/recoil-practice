48dd2788f60dae0f9f306718534e3caf86dc1d0e
yeonju <duswn4338@gmail.com>
2023-07-04 23:32:43 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { styled } from "styled-components";
 
+const Father = styled.div`
+  background-color: red;
+`;
+const Box = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Father>
+        <Box></Box>
+        <div>hidddddd</div>
+      </Father>
     </div>
   );
 }
