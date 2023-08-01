1f32263ba55310e1f98640cbeccc0a1b29bcb514
yeonju <duswn4338@gmail.com>
2023-08-02 00:11:46 +0900

    chore: coin type추가

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,37 +1,51 @@
-import { useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { ITag, InfoData, RouteState } from '../types/CoinType';
 
-interface RouteState {
-  state: {
-    name: string;
-  };
-}
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.div`
+  text-align: center;
+  color: ${(props) => props.theme.accentColor};
+`;
+
 function Coin() {
   const [loading, setLoading] = useState(true);
   const { state } = useLocation() as RouteState;
-  console.log(state.name);
-  // const { coinId } = useParams();
-  const Container = styled.div`
-    padding: 0px 20px;
-    max-width: 480px;
-    margin: 0 auto;
-  `;
-  const Header = styled.header`
-    height: 10vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
-  const Title = styled.h1`
-    font-size: 48px;
-    color: ${(props) => props.theme.accentColor};
-  `;
+  const [coin, setCoin] = useState();
+  const { coinId } = useParams();
+  const [ihfo, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+  useEffect(() => {
+    (async () => {
+      const infoData = await( await fetch(
+        `https://api.coinpaprika.com/v1/coins/${coinId}`
+      )).json()
+      const priceData =  await( await fetch(
+        `https://api.coinpaprika.com/v1/tickers/${coinId}`
+      )).json()
+      console.log(priceData)
 
-  const Loader = styled.div`
-    text-align: center;
-    color: ${(props) => props.theme.accentColor};
-  `;
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, [coinId]);
   return (
     <Container>
       <Header>
---
+++ src/types/CoinType.ts
@@ -0,0 +1,64 @@
+export interface RouteState {
+  state: {
+    name: string;
+  };
+}
+
+export interface ITag {
+  id: string;
+  name: string;
+  coin_counter: number;
+  ico_counter: number;
+}
+export interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+export interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    price: number;
+    volume_24h: number;
+    volume_24h_change_24h: number;
+    market_cap: number;
+    market_cap_change_24h: number;
+    percent_change_15m: number;
+    percent_change_30m: number;
+    percent_change_1h: number;
+    percent_change_6h: number;
+    percent_change_12h: number;
+    percent_change_1y: number;
+    percent_change_24h: number;
+    percent_change_7d: number;
+    percent_change_30d: number;
+    ath_price: number;
+    ath_date: number;
+    percent_from_price_ath: number;
+  };
+}
