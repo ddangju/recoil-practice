0285ed8db56f3bfe33806b9ceb6ccf193d71bbed
yeonju <duswn4338@gmail.com>
2023-08-02 23:37:15 +0900

    chore: nesting router

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,8 +1,7 @@
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useParams, Link, Outlet } from "react-router-dom";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { ITag, InfoData, RouteState } from '../types/CoinType';
-
+import { ITag, InfoData, RouteState, PriceData } from "../types/CoinType";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -24,34 +23,88 @@ const Loader = styled.div`
   text-align: center;
   color: ${(props) => props.theme.accentColor};
 `;
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
 
 function Coin() {
   const [loading, setLoading] = useState(true);
   const { state } = useLocation() as RouteState;
-  const [coin, setCoin] = useState();
   const { coinId } = useParams();
-  const [ihfo, setInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [coin, setCoin] = useState();
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
   useEffect(() => {
     (async () => {
-      const infoData = await( await fetch(
-        `https://api.coinpaprika.com/v1/coins/${coinId}`
-      )).json()
-      const priceData =  await( await fetch(
-        `https://api.coinpaprika.com/v1/tickers/${coinId}`
-      )).json()
-      console.log(priceData)
-
-      setInfo(infoData);
-      setPriceInfo(priceData);
+      // const infoData = await (
+      //   await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      // ).json();
+      // const priceData = await (
+      //   await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      // ).json();
+      // setInfo(infoData);
+      // setPriceInfo(priceData);
     })();
-  }, [coinId]);
+  }, [coinId, priceInfo]);
   return (
     <Container>
       <Header>
-        <Title>코인{state?.name || "Loading..."}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+        </Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : null}
+      {/* {loading ? (
+        <Loader>Loading...</Loader>
+      ) : ( */}
+      <>
+        <Overview>
+          <OverviewItem>
+            <span>Rank:</span>
+            <span>{info?.rank}</span>
+          </OverviewItem>
+          <OverviewItem>
+            <span>Symbol:</span>
+            <span>${info?.symbol}</span>
+          </OverviewItem>
+          <OverviewItem>
+            <span>Open Source:</span>
+            <span>{info?.open_source ? "Yes" : "No"}</span>
+          </OverviewItem>
+        </Overview>
+        <Description>{info?.description}</Description>
+        <Overview>
+          <OverviewItem>
+            <span>Total Supply:</span>
+            <span>{priceInfo?.total_supply}</span>
+          </OverviewItem>
+          <OverviewItem>
+            <span>Max Supply:</span>
+            <span>{priceInfo?.max_supply}</span>
+          </OverviewItem>
+        </Overview>
+        <Link to="price">price</Link>
+        <Link to="chart">chart</Link>
+        <Outlet></Outlet>
+      </>
+      {/* )} */}
     </Container>
   );
 }
--- src/types/CoinType.ts
+++ src/types/CoinType.ts
@@ -43,22 +43,24 @@ export interface PriceData {
   first_data_at: string;
   last_updated: string;
   quotes: {
-    price: number;
-    volume_24h: number;
-    volume_24h_change_24h: number;
-    market_cap: number;
-    market_cap_change_24h: number;
-    percent_change_15m: number;
-    percent_change_30m: number;
-    percent_change_1h: number;
-    percent_change_6h: number;
-    percent_change_12h: number;
-    percent_change_1y: number;
-    percent_change_24h: number;
-    percent_change_7d: number;
-    percent_change_30d: number;
-    ath_price: number;
-    ath_date: number;
-    percent_from_price_ath: number;
+    USD: {
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_15m: number;
+      percent_change_30m: number;
+      percent_change_1h: number;
+      percent_change_6h: number;
+      percent_change_12h: number;
+      percent_change_1y: number;
+      percent_change_24h: number;
+      percent_change_7d: number;
+      percent_change_30d: number;
+      ath_price: number;
+      ath_date: number;
+      percent_from_price_ath: number;
+    };
   };
 }
