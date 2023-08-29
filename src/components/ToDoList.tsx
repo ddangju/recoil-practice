8350f03a0ebbe624ca42e83918adc12e14e5406c
yeonju <duswn4338@gmail.com>
2023-08-29 22:54:36 +0900

    chore: selector refactoring

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,19 +1,43 @@
-import { toDoState, toDoSelector } from "../store/atoms";
-import { useRecoilValue } from "recoil";
+import { toDoState, toDoSelector, categoryState } from "../store/atoms";
+import { useRecoilState, useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-  const [toDo, doing, done] = useRecoilValue(toDoSelector);
+  //✅state값을 가져올때
+  // const toDos = useRecoilValue(toDoState);
+  // ✅selector에서 변형되는 값들을 가져올때
+  // const [todo, doing, done] = useRecoilValue(toDoSelector);
+  // ✅state, setState 사용할때
+  const [category, setCategory] = useRecoilState(categoryState);
+  const toDos = useRecoilValue(toDoSelector);
+
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
+  console.log(category);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">todo</option>
+        <option value="DOING">doing</option>
+        <option value="DONE ">done</option>
+      </select>
       <CreateToDo></CreateToDo>
-      <h2>To Do</h2>
-      <ul>
-        {toDo.map((toDo) => (
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
+    </div>
+  );
+}
+
+export default ToDoList;
+
+/* 
+<ul>
+        {todo.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
@@ -31,14 +55,10 @@ function ToDoList() {
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
-      <hr />
-      {/* <ul>
+      <hr /> */
+
+/* <ul>
         {toDos.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
-      </ul> */}
-    </div>
-  );
-}
-
-export default ToDoList;
+      </ul> */
--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -4,6 +4,12 @@ export interface IToDo {
   id: number;
   category: "TO_DO" | "DOING" | "DONE";
 }
+//사용자가 현재 선택한 카테고리에 대한 state
+///select state
+export const categoryState = atom({
+  key: "category",
+  default: "TO_DO",
+});
 
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
@@ -14,13 +20,12 @@ export const toDoState = atom<IToDo[]>({
 //atom의 output을 변형시키는 도구
 //get은 atom을 받을 수 있다
 //state를 바꾸지 않고 output을 결정한다
+
 export const toDoSelector = selector({
   key: "toDoSelector",
   get: ({ get }) => {
     const toDos = get(toDoState);
-    let toDo = toDos.filter((toDo) => toDo.category === "TO_DO");
-    let doing = toDos.filter((toDo) => toDo.category === "DOING");
-    let done = toDos.filter((toDo) => toDo.category === "DONE");
-    return [toDo, doing, done];
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
   },
 });
